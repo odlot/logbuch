d3f2c0c6444089bc0eb7b74d610a11e385779cae
odlot <131015417+odlot@users.noreply.github.com>
2025-11-28 20:53:45 +0100

    Initializr generated spring boot project

---
+++ src/main/java/io/github/odlot/logbuch/LogbuchApplication.java
@@ -0,0 +1,13 @@
+package io.github.odlot.logbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/odlot/logbuch/LogbuchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.odlot.logbuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
